a69e1de17e65759bab334ed2594bb47710dbb7a0
Dao Vo <votrongdao@gmail.com>
2024-09-07 21:31:49 +0700

    filter by ofType
    filter by ofType

--- linq.js
+++ linq.js
@@ -19,6 +19,41 @@
     Array.prototype.select = function (selector) {
         return this.map(selector);
     };
+
+    Array.prototype.orderBy = function (selector) {
+        return this.sort((a, b) => {
+            return selector(a) > selector(b) ? 1 : -1;
+        });
+    };
+
+    Array.prototype.orderByDescending = function (selector) {
+        return this.sort((a, b) => {
+            return selector(a) < selector(b) ? 1 : -1;
+        });
+    };
+
+    Array.prototype.ofType = function (type) {
+        return this.filter(function (item) {
+            if (type === null) {
+                return item === null;
+            }
+            if (type === undefined) {
+                return typeof item === 'undefined';
+            }
+            if (type === Array) {
+                return Array.isArray(item);
+            }
+            if (type === Object && typeof item === 'object' && item !== null && !Array.isArray(item)) {
+                // Handle plain objects
+                return item.constructor === Object;
+            }
+            if (typeof type === 'function') {
+                return item instanceof type || typeof item === type.name.toLowerCase();
+            }
+            return false;
+        });
+    };
+
 })();
 
 // String extensions
